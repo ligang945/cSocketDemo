8392a4c415d1f62f5332b90cb57d0a5cb156cd1b
ligang945 <ligang945@users.noreply.github.com>
2018-12-23 13:31:16 +0800

    format

--- client.cpp
+++ client.cpp
@@ -10,34 +10,33 @@
 #define MAXLINE 80
 #define SERV_PORT 1234
 
-int main(int argc, char *argv[])
-{
-	struct sockaddr_in servaddr;
-	char buf[MAXLINE];
-	int sockfd, n;
-	char *str;
-    
-	if (argc != 2) {
-		fputs("usage: ./client message\n", stderr);
-		exit(1);
-	}
-	str = argv[1];
-    
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+int main(int argc, char *argv[]) {
+    struct sockaddr_in servaddr;
+    char buf[MAXLINE];
+    int sockfd, n;
+    char *str;
 
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
-	servaddr.sin_port = htons(SERV_PORT);
-    
-	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (argc != 2) {
+        fputs("usage: ./client message\n", stderr);
+        exit(1);
+    }
+    str = argv[1];
 
-	write(sockfd, str, strlen(str));
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	n = read(sockfd, buf, MAXLINE);
-	printf("Response from server:\n");
-	write(STDOUT_FILENO, buf, n);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    servaddr.sin_port = htons(SERV_PORT);
 
-	close(sockfd);
-	return 0;
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    write(sockfd, str, strlen(str));
+
+    n = read(sockfd, buf, MAXLINE);
+    printf("Response from server:\n");
+    write(STDOUT_FILENO, buf, n);
+
+    close(sockfd);
+    return 0;
 }
